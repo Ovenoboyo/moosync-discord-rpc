d9895cb04941d445f6fe81e6eda2b009bfab5ac3
Sahil Gupte <sahilsachingupte@gmail.com>
2022-03-17 19:22:48 +0530

    Remove unnecessary packages
    Signed-off-by: Sahil Gupte <sahilsachingupte@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 var path = require('path');
-const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: './src/index.ts',
     target: "node",
-    mode: "development",
-    devtool: 'source-map',
+    mode: "production",
+    devtool: this.mode === 'development' ? 'source-map' : undefined,
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
@@ -20,16 +19,7 @@ module.exports = {
             {
                 use: 'ts-loader',
                 test: /\.ts?$/
-            },
-            {
-                test: /\.node$/,
-                loader: "node-loader",
-            },
+            }
         ]
-    },
-    plugins: [new CopyPlugin({
-        patterns: [
-            { from: "node_modules/discord-game/build/Release/discord_game.node", to: '../build/Release/discord_game.node' },
-        ],
-    }),]
+    }
 }
